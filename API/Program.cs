206b89d13ba15f653cd4a8e1e58afb77b527eff0
Bastian Asmussen <bastian@asmussen.tech>
2024-02-22 13:02:32 +0100

    TODO: Revert CORS override.

--- API/Program.cs
+++ API/Program.cs
@@ -6,11 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
     var services = builder.Services;
 
     services.AddDbContext<DataContext>();
+    services.AddControllers();
     services.AddEndpointsApiExplorer();
     services.AddSwaggerGen();
 }
 
 var app = builder.Build();
+
+app.UseCors(b => b
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+);
 
 if (app.Environment.IsDevelopment())
 {
@@ -19,4 +26,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 app.Run();

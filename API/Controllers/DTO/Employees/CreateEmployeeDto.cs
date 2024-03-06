8566743e8ebcd0bc6cbec67ded4ca5836a361061
Bastian Asmussen <bastian@asmussen.tech>
2024-03-06 13:00:08 +0100

    Remove DTOs.

--- API/Controllers/DTO/Employees/CreateEmployeeDto.cs
+++
@@ -1,9 +0,0 @@
-using API.Controllers.DTO.Users;
-
-namespace API.Controllers.DTO.Employees;
-
-public class CreateEmployeeDto : BaseEmployeeDto
-{
-    public int? ManagerId { get; set; }
-    public int UserId { get; set; }
-}
--- API/Controllers/DTO/Products/CreateProductDto.cs
+++
@@ -1,9 +0,0 @@
-namespace API.Controllers.DTO.Products;
-
-public class CreateProductDto : BaseProductDto
-{
-    public string Name { get; set; }
-    public string? Description { get; set; }
-
-    public decimal Price { get; set; }
-}
